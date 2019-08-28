3893001616cf073b6916f9e1a8d8d506e816dea8
RamaVchandran <ramav660@gmail.com>
2019-08-28 07:47:28 +0530

    mmm

--- src/test/java/org/stepdefinition/TestRunner.java
+++ src/test/java/org/stepdefinition/TestRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",
                  glue="org.stepdefinition",
-                  plugin= {"html:target","rerun:src/test/resources/failed.txt"},
+                  plugin= {"html:target","rerun:src/test/resources/failed.txt","json:target/report.json"},
                   monochrome=true,
                   dryRun=false,
                   strict=true,
--- target/report.js
+++ target/report.js
@@ -57,7 +57,8 @@ formatter.match({
   "location": "AddCustomerSteps.user_enters_all_the_fields()"
 });
 formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: target window already closed\nfrom unknown error: web view not found\n  (Session info: chrome\u003d76.0.3809.100)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DHAMO-PC\u0027, ip: \u0027192.168.0.110\u0027, os.name: \u0027Windows 7\u0027, os.arch: \u0027amd64\u0027, os.version: \u00276.1\u0027, java.version: \u00271.8.0_181\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 76.0.3809.100, chrome: {chromedriverVersion: 75.0.3770.90 (a6dcaf7e3ec6f..., userDataDir: C:\\Users\\Dhamo\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:57624}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 54a96a6e473313fab88e84ad1d8d9f33\n*** Element info: {Using\u003did, value\u003dfname}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementById(RemoteWebDriver.java:372)\r\n\tat org.openqa.selenium.By$ById.findElement(By.java:188)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy16.sendKeys(Unknown Source)\r\n\tat org.resources.FunctionalLibrary.insertValue(FunctionalLibrary.java:22)\r\n\tat org.stepdefinition.AddCustomerSteps.user_enters_all_the_fields(AddCustomerSteps.java:47)\r\n\tat ✽.user enters all the fields(src/test/resources/Features/AddCustomer.feature:11)\r\n",
+  "status": "failed"
 });
 formatter.step({
   "name": "user click on submit button",
@@ -67,7 +68,7 @@ formatter.match({
   "location": "AddCustomerSteps.user_click_on_submit_button()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "user should be displayed the customer id is generated",
@@ -77,7 +78,7 @@ formatter.match({
   "location": "AddCustomerSteps.user_should_be_displayed_the_customer_id_is_generated()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
   "status": "passed"
